98bebafd8ca0219beb956be9cf689fca73581021
Chris Polis <cmpolis@gmail.com>
2011-09-15 15:57:41 -0700

    more comments

--- pagify.js
+++ pagify.js
@@ -23,9 +23,12 @@
     var runAfterLoading = function() {
       self.switchPage = function(page) {
         page = page || window.location.hash.replace('#','');
-      
+     
+        // Load page content from cache 
         if(self.settings.cache) {
           $(self).hide().html(self.pages[page])[self.settings.animation]();
+ 
+        // Fetch page content
         } else {
           $.get(page+'.html', function(content) {
             $(self).hide().html(content)[self.settings.animation]();
