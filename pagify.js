3d5bf325f0881c1daf1e3a2f6a15064943f7bf33
Chris Polis <cmpolis@gmail.com>
2011-09-15 14:48:08 -0700

    first functional version of pagify.js!

--- pagify.js
+++ pagify.js
@@ -6,3 +6,32 @@
  *
  * Copyright (c) 2011, Chris Polis
  */
+
+(function($) {
+  $.fn.pagify = function(options) { 
+    var self = this;
+  
+    this.defaults = {
+      pages: [],
+      default: null,
+      animation: 'show',
+      cache: false
+    };
+    this.settings = $.extend({}, this.defaults, options);
+
+    this.switchPage = function(page) {
+      page = page || window.location.hash.replace('#','');
+      $(self).load(page+'.html');
+    }
+
+    // Respond to hash changes
+    $(window).bind('hashchange', function() {
+      self.switchPage();
+    });
+
+    // Load initial page - current hash or default page
+    if(window.location.hash) self.switchPage();
+    else if(this.settings.default) self.switchPage(this.settings.default);
+  };
+  
+})(jQuery);
