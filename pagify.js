017beaae160c9d773173d606523a935b72404ea6
Chris Polis <cmpolis@gmail.com>
2011-09-14 18:42:23 -0700

    added js files to demo, empty pagify.js

---
+++ pagify.js
@@ -0,0 +1,8 @@
+/*
+ * Pagify - A jquery plugin for effortlessly creating single page web sites.
+ *
+ * Licensed under the MIT:
+ * http://www.opensource.org/licenses/mit-license.php
+ *
+ * Copyright (c) 2011, Chris Polis
+ */
